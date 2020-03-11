99ba91a1383b5ee4a7580ab1431830dbf6da4772
liuyangming <liuyangming@bignox.com>
2020-03-11 17:11:00 +0800

    Glide+okhttp

--- baselib/src/main/java/com/quickstart/baselib/util/ImageUtil.java
+++ baselib/src/main/java/com/quickstart/baselib/util/ImageUtil.java
@@ -1,6 +1,12 @@
 package com.quickstart.baselib.util;
 
-import android.util.Log;
+import android.content.Context;
+import android.widget.ImageView;
+
+import com.quickstart.baselib.image.GlideApp;
 
 public class ImageUtil {
+    public static void show(Context context, String url, ImageView iv) {
+        GlideApp.with(context).load(url).into(iv);
+    }
 }
