0e1902e5fd5d0a22b0e1130ba88cf6acd5d633ca
liuyangming <liuyangming@bignox.com>
2020-03-11 16:43:09 +0800

    修改java8引用

--- testmodule/src/main/java/com/lym/testmodule/MainActivity.java
+++ testmodule/src/main/java/com/lym/testmodule/MainActivity.java
@@ -1,8 +1,13 @@
 package com.lym.testmodule;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.bumptech.glide.Glide;
+
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        ImageView iv = findViewById(R.id.iv);
+        Glide.with(this).load("https://raw.githubusercontent.com/liuyangming1993/BlogPic/master/1.png").into(iv);
     }
 }
