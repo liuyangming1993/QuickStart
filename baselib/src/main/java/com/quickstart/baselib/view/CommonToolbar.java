b46d461d28d97b0a0d4811af2014c89c276c4a18
liuyangming <liuyangming@bignox.com>
2020-03-23 21:00:51 +0800

    优化代码

--- baselib/src/main/java/com/quickstart/baselib/view/CommonToolbar.java
+++ baselib/src/main/java/com/quickstart/baselib/view/CommonToolbar.java
@@ -2,9 +2,20 @@ package com.quickstart.baselib.view;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.quickstart.baselib.R;
+import com.quickstart.baselib.base.BaseActivity;
 
 public class CommonToolbar extends RelativeLayout {
+    private ImageView mIvLeft;
+    private TextView mTvTitle;
+    private RelativeLayout mRl;
+
     public CommonToolbar(Context context) {
         this(context, null, 0);
     }
@@ -15,10 +26,21 @@ public class CommonToolbar extends RelativeLayout {
 
     public CommonToolbar(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-        initView();
+        initView(context);
+        initListener();
     }
 
-    private void initView() {
+    private void initListener() {
+        mIvLeft.setOnClickListener(v -> {
+            if (v.getContext() instanceof BaseActivity) {
+                ((BaseActivity) v.getContext()).finish();
+            }
+        });
+    }
 
+    private void initView(Context context) {
+        mRl = (RelativeLayout) LayoutInflater.from(context).inflate(R.layout.view_common_toolbar, this, true);
+        mIvLeft = mRl.findViewById(R.id.iv_left);
+        mTvTitle = mRl.findViewById(R.id.tv_title);
     }
 }
