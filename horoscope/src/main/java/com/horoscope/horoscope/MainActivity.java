b3c0f0f51b102cab65acae2950ed8889194d79d4
liuyangming <liuyangming@bignox.com>
2020-04-21 19:44:07 +0800

    增加Glide高斯模糊特效

--- horoscope/src/main/java/com/horoscope/horoscope/MainActivity.java
+++ horoscope/src/main/java/com/horoscope/horoscope/MainActivity.java
@@ -1,9 +1,14 @@
 package com.horoscope.horoscope;
 
+import android.widget.ImageView;
+
+import com.bumptech.glide.request.RequestOptions;
 import com.horoscope.horoscope.mvp.contract.MainContract;
 import com.horoscope.horoscope.mvp.m.MainModel;
 import com.horoscope.horoscope.mvp.p.MainPresenter;
 import com.quickstart.baselib.base.BaseActivity;
+import com.quickstart.baselib.image.GlideApp;
+import com.quickstart.baselib.util.BlurTransformation;
 
 public class MainActivity extends BaseActivity<MainPresenter> implements MainContract.MainView {
 
@@ -19,6 +24,11 @@ public class MainActivity extends BaseActivity<MainPresenter> implements MainCon
 
     @Override
     public void initView() {
+        ImageView iv = findViewById(R.id.iv);
+        GlideApp.with(this)
+                .load("https://www.baidu.com/img/bd_logo1.png")
+                .apply(RequestOptions.bitmapTransform(new BlurTransformation(this, 30, 3)))
+                .into(iv);
     }
 
     @Override
